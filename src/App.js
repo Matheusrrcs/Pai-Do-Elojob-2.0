7947e5ba666a53e772a0584348949c1ce4ca1d03
Matheusrrcs <matheus-santana-msn@hotmail.com>
2022-07-09 18:46:06 -0300

    feedback dos clientes e responsividade

--- src/App.js
+++ src/App.js
@@ -33,8 +33,10 @@ function App() {
 
       </Routes>
 
-      <button class="scroll-top scroll-to-target open" data-target="html" data-aos="fade-down" data-aos-once="false" onClick={() => {
-        $('html, body').animate({ scrollTop: 0 }, 1000);
+      <button class="scroll-top scroll-to-target open" data-target="html" data-aos="fade-down" data-aos-once="false" data-aos-anchor=".conteudo-icone" onClick={() => {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 1000);;
       }}>
         <i class="fas fa-angle-up" ></i>
       </button>
